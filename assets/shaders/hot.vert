68cd190c09506580f6d71b6bb54ac631e781bfb8
Jonas Matser <github@jonasmatser.nl>
2021-05-17 21:05:11 +0200

    Test terrain shader stuff

--- assets/shaders/hot.vert
+++ assets/shaders/hot.vert
@@ -1,15 +1,27 @@
 #version 450
 
 layout(location = 0) in vec3 Vertex_Position;
+layout(location = 1) in vec3 Vertex_Normal;
 
 layout(set = 0, binding = 0) uniform CameraViewProj {
     mat4 ViewProj;
 };
 
-layout(set = 1, binding = 0) uniform Transform {
+layout(set = 2, binding = 0) uniform Transform {
     mat4 Model;
 };
 
+layout(location = 0) out vec3 v_WorldPosition;
+layout(location = 1) out vec3 v_WorldNormal;
+layout(location = 2) out vec2 v_Uv;
+
 void main() {
-    gl_Position = ViewProj * Model * vec4(Vertex_Position, 1.0);
+    vec4 world_position = Model * vec4(Vertex_Position, 1.0);
+    v_WorldPosition = world_position.xyz;
+
+    v_WorldNormal = mat3(Model) * Vertex_Normal;
+
+    v_Uv = vec2(0.5, world_position.y / 3);
+
+    gl_Position = ViewProj * world_position;
 }
--- examples/shader/hot_shader_reloading.rs
+++ examples/shader/hot_shader_reloading.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    asset::LoadState,
     prelude::*,
     reflect::TypeUuid,
     render::{
@@ -7,74 +8,158 @@ use bevy::{
         render_graph::{base, AssetRenderResourcesNode, RenderGraph},
         renderer::RenderResources,
         shader::ShaderStages,
+        texture::{self, AddressMode},
     },
 };
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum AppState {
+    Setup,
+    Finished,
+}
 
 /// This example illustrates how to load shaders such that they can be
 /// edited while the example is still running.
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_asset::<MyMaterial>()
-        .add_startup_system(setup.system())
+        .add_state(AppState::Setup)
+        .add_system_set(
+            SystemSet::on_enter(AppState::Setup).with_system(load_terrain_assets.system()),
+        )
+        .add_system_set(
+            SystemSet::on_update(AppState::Setup).with_system(check_terrain_assets.system()),
+        )
+        .add_system_set(SystemSet::on_enter(AppState::Finished).with_system(setup.system()))
+        .add_system(rotator_system.system())
         .run();
 }
 
-#[derive(RenderResources, Default, TypeUuid)]
-#[uuid = "3bf9e364-f29d-4d6c-92cf-93298466c620"]
-struct MyMaterial {
-    pub color: Color,
+struct TerrainAssets {
+    texture: Handle<Texture>,
+    vs: Handle<Shader>,
+    fs: Handle<Shader>,
+}
+
+impl TerrainAssets {
+    fn as_vec(&self) -> Vec<HandleUntyped> {
+        vec![
+            self.texture.clone_untyped(),
+            self.vs.clone_untyped(),
+            self.fs.clone_untyped(),
+        ]
+    }
+}
+
+fn load_terrain_assets(mut commands: Commands, asset_server: ResMut<AssetServer>) {
+    asset_server.watch_for_changes().unwrap();
+
+    let terrain_assets = TerrainAssets {
+        texture: asset_server.load("textures/terrain_LUT.png"),
+        vs: asset_server.load("shaders/hot.vert"),
+        fs: asset_server.load("shaders/hot.frag"),
+    };
+    commands.insert_resource(terrain_assets);
+}
+
+fn check_terrain_assets(
+    terrain_assets: Res<TerrainAssets>,
+    mut state: ResMut<State<AppState>>,
+    asset_server: Res<AssetServer>,
+) {
+    match asset_server
+        .get_group_load_state(terrain_assets.as_vec().into_iter().map(|handle| handle.id))
+    {
+        LoadState::Loaded => state.set(AppState::Finished).unwrap(),
+        LoadState::Loading | LoadState::NotLoaded => {}
+        LoadState::Failed => panic!(),
+    }
 }
 
 fn setup(
     mut commands: Commands,
-    asset_server: ResMut<AssetServer>,
     mut pipelines: ResMut<Assets<PipelineDescriptor>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<MyMaterial>>,
-    mut render_graph: ResMut<RenderGraph>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut textures: ResMut<Assets<Texture>>,
+    terrain_assets: Res<TerrainAssets>,
 ) {
-    // Watch for changes
-    asset_server.watch_for_changes().unwrap();
-
     // Create a new shader pipeline with shaders loaded from the asset directory
-    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
-        vertex: asset_server.load::<Shader, _>("shaders/hot.vert"),
-        fragment: Some(asset_server.load::<Shader, _>("shaders/hot.frag")),
-    }));
+    let mut pipe = PipelineDescriptor::default_config(ShaderStages {
+        vertex: terrain_assets.vs.clone(),
+        fragment: Some(terrain_assets.fs.clone()),
+    });
+    pipe.primitive.cull_mode = None;
+    let pipeline_handle = pipelines.add(pipe);
 
-    // Add an AssetRenderResourcesNode to our Render Graph. This will bind MyMaterial resources to
-    // our shader
-    render_graph.add_system_node(
-        "my_material",
-        AssetRenderResourcesNode::<MyMaterial>::new(true),
-    );
+    let mut texture = textures.get_mut(terrain_assets.texture.clone()).unwrap();
+    texture.sampler.address_mode_v = AddressMode::Repeat;
 
-    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This
-    // ensures "my_material" runs before the main pass
-    render_graph
-        .add_node_edge("my_material", base::node::MAIN_PASS)
-        .unwrap();
-
-    // Create a new material
-    let material = materials.add(MyMaterial {
-        color: Color::rgb(0.0, 0.8, 0.0),
+    let material = materials.add(StandardMaterial {
+        base_color: Color::WHITE,
+        base_color_texture: Some(terrain_assets.texture.clone()),
+        roughness: 1.0,
+        metallic: 0.0,
+        ..Default::default()
     });
 
     // cube
-    commands
-        .spawn_bundle(MeshBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
-            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
-                pipeline_handle,
-            )]),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..Default::default()
-        })
-        .insert(material);
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(3.0, 5.0, -8.0).looking_at(Vec3::ZERO, Vec3::Y),
+    // commands.spawn_bundle(PbrBundle {
+    //     mesh: meshes.add(Mesh::from(shape::Torus {
+    //         radius: 10.0,
+    //         ring_radius: 5.0,
+    //         subdivisions_segments: 100,
+    //         subdivisions_sides: 10,
+    //     })),
+    //     render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
+    //         pipeline_handle,
+    //     )]),
+    //     transform: Transform::from_xyz(0.0, 0.0, 0.0),
+    //     material,
+    //     ..Default::default()
+    // });
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere {
+            radius: 10.0,
+            subdivisions: 10,
+        })),
+        render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
+            pipeline_handle,
+        )]),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        material,
         ..Default::default()
     });
+
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 20000.0,
+            range: 2000.0,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0.0, 20.0, 50.0),
+        ..Default::default()
+    });
+
+    // camera
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_xyz(50.0, 25.0, -50.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert(Rotates);
+}
+
+/// this component indicates what entities should rotate
+struct Rotates;
+
+fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in query.iter_mut() {
+        *transform = Transform::from_rotation(
+            Quat::from_rotation_y((4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds())
+                * Quat::from_rotation_z((4.0 * std::f32::consts::PI / 40.0) * time.delta_seconds()),
+        ) * *transform;
+    }
 }
